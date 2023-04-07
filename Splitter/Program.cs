0958e0f08ad5ba5dcdb28a9a39a020614c68a1b4
Paul Thomson <Pthomson@wgeld.org>
2023-04-07 11:10:40 -0400

    no changes, committing in order to pull

--- Splitter/Program.cs
+++ Splitter/Program.cs
@@ -6,20 +6,20 @@ int elementsPerFile = 700;
 int index = 0;
 string saveLocation = "Z:\\IT_Development\\Projects\\Active\\MDMIntervalDataParcer\\Test_Output";
 
-XDocument xdoc = XDocument.Load(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\SourceFiles\AMIPRDAP17_DAILY_LP_04_07_2023_06_40_33.xml");
-xdoc.Descendants("MetersNotRead").Remove();
-xdoc.Descendants("MetersRead").Remove();
-xdoc.Descendants("ScheduleExecution").Remove();
+XDocument xDoc1 = XDocument.Load(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\SourceFiles\AMIPRDAP17_DAILY_LP_04_07_2023_06_40_33.xml");
+xDoc1.Descendants("MetersNotRead").Remove();
+xDoc1.Descendants("MetersRead").Remove();
+xDoc1.Descendants("ScheduleExecution").Remove();
 
 
 //counts number of elements in XML file, limits the size(number of elements) of the new split file to make it so it fits in 24 files.
-foreach (var element in xdoc.Root.Elements())
+foreach (var element in xDoc1.Root.Elements())
 {
     elementCount++;
 }
 elementsPerFile = (elementCount / 24) + 20;
 
-foreach (var batch in xdoc.Root.Elements().InSetsOf(elementsPerFile))
+foreach (var batch in xDoc1.Root.Elements().InSetsOf(elementsPerFile))
 {
     var newDoc = new XDocument(
          new XElement("AMRDEF", batch));
