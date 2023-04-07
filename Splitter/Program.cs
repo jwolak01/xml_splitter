79e81470c0b46bbd668fc30ef11e6ec7d7b85351
Wolak <Jwolak@wgeld.org>
2023-04-07 09:06:36 -0400

    add code to project. this allows the system to find a file in a specified path, manipulate that file, and save it as a temp.xml file

--- Splitter/Program.cs
+++ Splitter/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Jim!");
+﻿using System.Xml.Linq;
+
+XDocument doc1 = XDocument.Load(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\AMIPRDAP17_DAILY_LP_04_04_2023_06_55_32.xml");
+XDocument doc2 = new XDocument(
+         new XElement("import",
+               doc1.Descendants("MeterReadings").Skip(1).Take(2))
+          );
+
+doc2.Save(@"C:\Users\jwolak\Desktop\temp.xml");
