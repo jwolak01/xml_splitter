babfe35e354b96f11e611437e3be63fb62716c88
Wolak <Jwolak@wgeld.org>
2023-04-07 11:05:27 -0400

    add a methodology to source for a file related to the current day

--- Splitter/Program.cs
+++ Splitter/Program.cs
@@ -1,31 +1,46 @@
 ﻿using System;
 using System.Xml.Linq;
 
+DateTime today = DateTime.Today;
+
 int elementCount = 0;
 int elementsPerFile = 700;
 int index = 0;
 string saveLocation = "Z:\\IT_Development\\Projects\\Active\\MDMIntervalDataParcer\\Test_Output";
 
-XDocument xdoc = XDocument.Load(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\SourceFiles\AMIPRDAP17_DAILY_LP_04_07_2023_06_40_33.xml");
-xdoc.Descendants("MetersNotRead").Remove();
-xdoc.Descendants("MetersRead").Remove();
-xdoc.Descendants("ScheduleExecution").Remove();
-
-
-//counts number of elements in XML file, limits the size(number of elements) of the new split file to make it so it fits in 24 files.
-foreach (var element in xdoc.Root.Elements())
+foreach (var path in Directory.GetFiles(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\SourceFiles"))
 {
-    elementCount++;
-}
-elementsPerFile = (elementCount / 24) + 20;
+    //Console.WriteLine(path); // full path
+    Console.WriteLine(System.IO.Path.GetFileName(path)); // file name
+    String date = System.IO.Path.GetFileName(path).Substring(20, 20);
+    date = date.Replace('_', '/');
+    //  Console.WriteLine(str.Substring(0,10));
 
-foreach (var batch in xdoc.Root.Elements().InSetsOf(elementsPerFile))
-{
-    var newDoc = new XDocument(
-         new XElement("AMRDEF", batch));
+    if (date.Substring(0, 10).Equals(today.ToString("MM/dd/yyyy")))
+    {
+        XDocument xdoc = XDocument.Load(@"Z:\IT_Development\Projects\Active\MDMIntervalDataParcer\SourceFiles\" + System.IO.Path.GetFileName(path));
+        xdoc.Descendants("MetersNotRead").Remove();
+        xdoc.Descendants("MetersRead").Remove();
+        xdoc.Descendants("ScheduleExecution").Remove();
 
-    newDoc.Save($"{saveLocation}\\exportedFile_{++index}.xml");
+
+        //counts number of elements in XML file, limits the size(number of elements) of the new split file to make it so it fits in 24 files.
+        foreach (var element in xdoc.Root.Elements())
+        {
+            elementCount++;
+        }
+        elementsPerFile = (elementCount / 24) + 20;
+
+        foreach (var batch in xdoc.Root.Elements().InSetsOf(elementsPerFile))
+        {
+            var newDoc = new XDocument(
+                 new XElement("AMRDEF", batch));
+
+            newDoc.Save($"{saveLocation}\\exportedFile_{++index}.xml");
+        }
+    }
 }
+
 
 public static class IEnumerableExtensions
 {
